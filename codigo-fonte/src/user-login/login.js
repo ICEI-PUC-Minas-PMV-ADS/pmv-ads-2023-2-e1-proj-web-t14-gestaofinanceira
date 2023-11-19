4f5a7b096400b6cc39136e2ddea523555b64716f
Miguel Costa <miguelsscaugusto@gmail.com>
2023-11-18 23:44:08 -0300

    feat: modified html, css e javascript

--- codigo-fonte/src/user-login/login.js
+++ codigo-fonte/src/user-login/login.js
@@ -26,3 +26,16 @@ function checkLogin(username, password) {
 function fetchUserData() {
     return JSON.parse(localStorage.getItem('user-data')) || { users: [] };
 }
+function abrir(){
+    document.getElementById('pop-up-esqueci-senha').style.display = 'flex';
+    document.getElementById('body').style.filter = 'blur(25px)'
+
+}
+function fechar(){
+    document.getElementById('pop-up-esqueci-senha').style.display = 'none';
+    document.getElementById('body').style.filter = 'none'
+
+}
+document.getElementById('input-cancelar').addEventListener('click', function() {
+    document.getElementById('input-email').required = false;
+  });

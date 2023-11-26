928d811394fcf3abd52feef86c06d53bf668a82f
Rodiney Branta <branta.rodiney@gmail.com>
2023-11-26 15:03:59 -0300

    feat: Implement header section with logo, greetings, and navigation
    - Added functionality to display the header logo
    - Implemented dynamic greeting messages
    - Set up navigation functionality in the header

--- codigo-fonte/js/script.js
+++ codigo-fonte/js/script.js
@@ -1,8 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const homeLink = document.getElementById('homeLink');
+document.addEventListener(`DOMContentLoaded`, function() {
+    const homeLink = document.getElementById(`homeLink`);
+    const headerLogo = document.getElementById(`headerLogo`);
 
     homeLink.addEventListener('click', function(event) {
         event.preventDefault();
-        window.location.href = 'index.html';
+        window.location.href = `index.html`;
     });
+
+    const imagePath = 'assets/images/logo-header.png';
+    headerLogo.src = imagePath;
+});
+
+document.addEventListener(`DOMContentLoaded`, function() {
+    const primaryHeader = document.getElementById(`greetingPrimary`);
+    const secondaryHeader = document.getElementById(`greetingSecondary`);
+
+    const currentUser = 'Eu mesmo';
+
+    primaryHeader.textContent = `Economus - Controle e Gerenciamento Financeiro Personalizado`;
+    secondaryHeader.textContent = `Bem-vindo(a), ${currentUser}`;
 });

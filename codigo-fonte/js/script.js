d2e46fcba3a6b0c804949363ce397cff2c159473
Rodiney Branta <branta.rodiney@gmail.com>
2023-11-26 21:53:21 -0300

    feature: Added side menu with styled links
    Description:
    - Implemented a side menu with styled links for user navigation.
    - Used CSS to create a black background with white text.
    - Each button has a dark gray background, black border, and hover
    effects for improved user interaction.

--- codigo-fonte/js/script.js
+++ codigo-fonte/js/script.js
@@ -12,11 +12,29 @@ document.addEventListener(`DOMContentLoaded`, function() {
 });
 
 document.addEventListener(`DOMContentLoaded`, function() {
-    const userProfileLik = document.getElementById(`usr-profile`);
+    const userProfileLink = document.getElementById(`usr-profile`);
 
-    const userProfilePageURL = ``;
+    const userProfilePageURL = `../user-profile/user-profile.html`;
 
-    userProfileLik.href = userProfilePageURL;
+    userProfileLink.href = userProfilePageURL;
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const userProfitLink = document.querySelector('.side-menu li:nth-child(1) a');
+    const userLossLink = document.querySelector('.side-menu li:nth-child(2) a');
+    const userGoalsLink = document.querySelector('.side-menu li:nth-child(3) a');
+    const userReportsLink = document.querySelector('.side-menu li:nth-child(4) a');
+
+
+    const userProfitURL = '../user-profit-statement/user-profit-statement.html';
+    const userLossURL = '../user-loss-statement/user-loss-statement.html';
+    const userGoalsURL = '../user-goals/user-goals.html';
+    const userReportsURL = '';
+
+    userProfitLink.href = userProfitURL;
+    userLossLink.href = userLossURL;
+    userGoalsLink.href = userGoalsURL;
+    userReportsLink.href = userReportsURL;
 });
 
 document.addEventListener(`DOMContentLoaded`, function() {

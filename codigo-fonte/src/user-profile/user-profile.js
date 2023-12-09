c751b20f28a60af479d25f6fabc6378cb5ccf449
Miguel Costa <miguelsscaugusto@gmail.com>
2023-12-09 09:58:57 -0300

    feat: modified html, css, javascript user-profiles

--- codigo-fonte/src/user-profile/user-profile.js
+++ codigo-fonte/src/user-profile/user-profile.js
@@ -1,56 +1,85 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const editBtn = document.getElementById('editBtn');
-    const saveBtn = document.getElementById('saveBtn');
-    const inputs = document.querySelectorAll('input[disabled]');
-  
-    editBtn.addEventListener('click', function() {
+function abrirEncerrar() {
+  document.getElementById('pop-up-encerrar').style.display = 'flex';
+  document.getElementById('body').style.filter = 'blur(25px)'
+
+}
+function fecharEncerrar() {
+  document.getElementById('pop-up-encerrar').style.display = 'none';
+  document.getElementById('body').style.filter = 'none'
+
+}
+document.addEventListener('DOMContentLoaded', function () {
+  const editBtn = document.getElementById('editBtn');
+  const saveBtn = document.getElementById('saveBtn');
+  const inputs = document.querySelectorAll('input[disabled]');
+
+  editBtn.addEventListener('click', function () {
       inputs.forEach(input => {
-        input.removeAttribute('disabled');
+          input.removeAttribute('disabled');
       });
       editBtn.style.display = 'none';
       saveBtn.style.display = 'block';
-    });
-  
-    const profileForm = document.getElementById('profileForm');
-    profileForm.addEventListener('submit', function(event) {
+  });
+
+  const profileForm = document.getElementById('profileForm');
+  profileForm.addEventListener('submit', function (event) {
       event.preventDefault();
       inputs.forEach(input => {
-        input.setAttribute('disabled', true);
+          input.setAttribute('disabled', true);
       });
       editBtn.style.display = 'block';
       saveBtn.style.display = 'none';
-  
+
       saveUserData();
-    });
-  
-    function saveUserData() {
+  });
+
+  function saveUserData() {
       const userData = fetchUserData();
       const currentUserIndex = getCurrentUserIndex(userData.users);
-  
+
       if (currentUserIndex > -1) {
-        userData.users[currentUserIndex].name = document.getElementById('name').value;
-        userData.users[currentUserIndex].email = document.getElementById('email').value;
-        userData.users[currentUserIndex].password = document.getElementById('password').value;
-  
-        localStorage.setItem('user-data', JSON.stringify(userData));
+          userData.users[currentUserIndex].name = document.getElementById('name').value;
+          userData.users[currentUserIndex].email = document.getElementById('email').value;
+          userData.users[currentUserIndex].password = document.getElementById('password').value;
+
+          localStorage.setItem('user-data', JSON.stringify(userData));
       }
-    }
-  
-    function fetchUserData() {
+  }
+
+  function fetchUserData() {
       return JSON.parse(localStorage.getItem('user-data')) || { users: [] };
-    }
-  
-    function getCurrentUserIndex(users) {
+  }
+
+  function getCurrentUserIndex(users) {
       const currentUserEmail = document.getElementById('email').value;
       return users.findIndex(user => user.email === currentUserEmail);
-    }
-  
-    // Populate user data
-    const userData = fetchUserData();
-    if (userData.users.length > 0) {
+  }
+
+  // Populate user data
+  const userData = fetchUserData();
+  if (userData.users.length > 0) {
       document.getElementById('name').value = userData.users[0].name;
       document.getElementById('email').value = userData.users[0].email;
       document.getElementById('password').value = userData.users[0].password;
-    }
+  }
+});
+function editarInputs() {
+  // Seletor para todos os inputs, ajuste conforme necessário
+  var inputs = document.querySelectorAll('input');
+
+  // Itera sobre cada input e define a background-color para #ffffff
+  inputs.forEach(function (input) {
+      input.style.backgroundColor = '#ffffff';
+      input.style.color = '#201B2C';
   });
-  
+}
+function deseditarInputs() {
+  // Seletor para todos os inputs, ajuste conforme necessário
+  var inputs = document.querySelectorAll('input');
+
+  // Itera sobre cada input e define a background-color para #ffffff
+  inputs.forEach(function (input) {
+      input.style.backgroundColor = '#514069';
+      input.style.color = '#ffffff';
+  });
+}
